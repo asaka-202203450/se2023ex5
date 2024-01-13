0687cff54a6eb4d4782692a7745ba56e52d64957
asaka ryo <r202203450aq@jindai.jp>
2024-01-13 15:54:14 +0900

    strlen

--- main1.c
+++ main1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-    char str[]="hello";
-    printf("%s", str);
-    return 0;
+int main(void)
+{
+	size_t len;
+	char hello[] = "Hello";
+
+	len = strlen(hello);
+	printf("%s:%ld \n", hello, len);
+
+	return 0;
 }
